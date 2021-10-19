8a36b8347ab43c154c1db293cee0e5c3611e8447
Charli199 <61799577+Charli199@users.noreply.github.com>
2021-10-19 17:22:02 -0500

    Proyecto bases

--- src/index.js
+++ src/index.js
@@ -40,5 +40,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Iniciar servidor
 app.listen(4000, () => {
-    console.log(`Server on port ${app.get('port')}`);
+    console.log(`Server on port ${app.get('port')}`);   
 });
